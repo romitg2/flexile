c66a18fab655592410a701e7a9ea51922ce10c01
Maya <19721695+MayaRainer@users.noreply.github.com>
2025-08-21 19:33:56 +0200

    Use more workers for Playwright on CI (#985)
    Co-authored-by: Maya <19721695+code-elf@users.noreply.github.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,8 +11,8 @@ export default defineConfig({
   testDir: "./e2e",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: 4,
+  retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? "100%" : undefined,
   reporter: process.env.CI ? [["list"], ["html"]] : "list",
   use: {
     baseURL: "https://test.flexile.dev:3101",
