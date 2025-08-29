df86dd771893b64e258bdaed258ca318508e139a
Sahil Lavingia <sahil.lavingia@gmail.com>
2025-08-29 11:41:42 -0400

    Update next.config.ts
    Signed-off-by: Sahil Lavingia <sahil.lavingia@gmail.com>

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -26,7 +26,7 @@ const nextConfig: NextConfig = {
   },
 };
 if (process.env.NODE_ENV === "development") {
-  nextConfig.images.remotePatterns.push({ protocol: "http", hostname: "localhost", port: "3001" });
+  nextConfig.images.remotePatterns.push({ protocol: "http", hostname: "localhost", port: "3000" });
 }
 
 const withBundleAnalyzer = NextBundleAnalyzer({
